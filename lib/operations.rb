fc2acc4f12156aa447a93090c9aeb9e250597607
Noelle Rolwes <nrolwes@gmail.com>
2018-09-09 01:23:47 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,16 @@
 def unsafe?(speed)
-  speed<60 && speed>40 ? false : true  
+  if speed < 60
+    true
+  elsif speed < 40
+    true
+  else
+    false 
 end
 
 
 
 def not_safe?(speed)
-	
+	speed<60 && speed>40 ? false : true 
 end
 	
 
